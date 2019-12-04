0f1e8a172f4cd1f2216c23f43ac0db3efa88c727
erbcreedok <erbcreedok@gmail.com>
2019-12-05 02:45:22 +0600

    добавил страницу бизнеса

--- commit_and_build.sh
+++ commit_and_build.sh
@@ -1,16 +1,15 @@
 git add .
-git commit -m $0
+git commit -m "$1"
 git push
 
 cd ../ehrle-build
 git pull
 
 cd ../ehrle
-nvm use 6
 gulp build
 
 cd ../ehrle-build
 git add .
-git commit -m $0
+git commit -m "$1"
 git push
 
