ce12431308d5faa2ebba42e2f878e04fd7ba1fff
erbcreedok <erbcreedok@gmail.com>
2020-01-29 20:41:47 +0600

    добавил обработчик 200/400 в php

--- sendform.php
+++ sendform.php
@@ -1,5 +1,9 @@
 <?php
 $q = $_SERVER['QUERY_STRING'];
-$url = "https://script.google.com/macros/s/AKfycbxDp-WXzfENlaitsN6ZGAbn1-ruO2V6eUpyGkZ1FBXuWlU3YOPG/exec".$q;
-echo $url;
-// file_get_contents("https://script.google.com/macros/s/AKfycbxDp-WXzfENlaitsN6ZGAbn1-ruO2V6eUpyGkZ1FBXuWlU3YOPG/exec".$q);
+$url = "https://script.google.com/macros/s/AKfycbxDp-WXzfENlaitsN6ZGAbn1-ruO2V6eUpyGkZ1FBXuWlU3YOPG/exec?".$q;
+$result = json_decode(file_get_contents($url))->result;
+if ($result == 'success') {
+    http_response_code(200);
+} else {
+    http_response_code(400);
+}
