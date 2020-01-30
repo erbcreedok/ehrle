08448e3cd61493fa4a5be7eacd87d891a5880644
yerbol <erbcreedok@gmail.com>
2020-01-30 16:57:52 +0600

    обноновил конÐстÐанты калькуляторы

--- sendform.php
+++ sendform.php
@@ -1,6 +1,6 @@
 <?php
 $q = $_SERVER['QUERY_STRING'];
-$url = "https://script.google.com/macros/s/AKfycbxDp-WXzfENlaitsN6ZGAbn1-ruO2V6eUpyGkZ1FBXuWlU3YOPG/exec?".$q;
+$url = "https://script.google.com/macros/s/AKfycbyMv9LcDQVTFb8Os4nQFONaxcYoLxEYyY_QqOawWXLtvHsqz8M0/exec?".$q;
 $result = json_decode(file_get_contents($url))->result;
 if ($result == 'success') {
     http_response_code(200);
