6216632adc4fda66c05048b55388684ac109452c
erbcreedok <erbcreedok@gmail.com>
2020-01-30 20:25:16 +0600

    Исправил текстовое обрамление

--- sendform.php
+++ sendform.php
@@ -3,7 +3,7 @@ $q = $_SERVER['QUERY_STRING'];
 $url = "https://script.google.com/macros/s/AKfycbyMv9LcDQVTFb8Os4nQFONaxcYoLxEYyY_QqOawWXLtvHsqz8M0/exec?".$q;
 $tg_api = '1003245073:AAEgjRgpNBx0a9d47g6PSGU4_OQrCcW6ur8';
 $chat_id = '-386742703';
-$message = urlencode('Заявка от: '.$_GET['name'].'. Телефон: '.$_GET['phone'].' Форма: '.$_GET['form']);
+$message = urlencode('Заявка: '.$_GET['name'].".\nТелефон: ".$_GET['phone']."\nФорма: ".$_GET['form']);
 $url_tg = "https://api.telegram.org/bot$tg_api/sendMessage?chat_id=$chat_id&text=$message";
 file_get_contents($url_tg);
 $result = json_decode(file_get_contents($url))->result;
