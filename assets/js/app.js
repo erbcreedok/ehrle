19b6d6a25e3d7af9546fa87f99531257de61390e
yerbol <erbcreedok@gmail.com>
2020-01-31 18:07:35 +0600

    update modal scroll behavior

--- assets/js/app.js
+++ assets/js/app.js
@@ -104,6 +104,18 @@ Vue.use(VueTheMask);
       }
     },
     methods: {
+      setModalOpenedClassToBody() {
+        document.body.classList.add('body-with_modal');
+      },
+      setModalClosedClassToBody() {
+        document.body.classList.remove('body-with_modal');
+      },
+      setMenuOpenedClassToBody() {
+        document.body.classList.add('body-with_menu');
+      },
+      setMenuClosedClassToBody() {
+        document.body.classList.remove('body-with_menu');
+      },
       setCalcResults(data) {
         this.calcResults = data;
       },
@@ -115,11 +127,11 @@ Vue.use(VueTheMask);
       },
       openCalcModal() {
         this.showCalcModal = true;
-        document.body.style.overflow = 'hidden';
+        this.setModalOpenedClassToBody();
       },
       closeCalcModal() {
         this.showCalcModal = false;
-        document.body.style.overflow = null;
+        this.setModalClosedClassToBody();
       },
       handleIsCalcFixed(width = this.windowWidth) {
         this.isCalcRequiresFixed = width < calculatorBreakpoint;
@@ -156,11 +168,11 @@ Vue.use(VueTheMask);
       },
       openMenu() {
         this.isMenuOpened = true;
-        document.body.style.overflow = 'hidden';
+        this.setMenuOpenedClassToBody();
       },
       closeMenu() {
         this.isMenuOpened = false;
-        document.body.style.overflow = null;
+        this.setMenuClosedClassToBody();
       },
       langButtonPressed(lang) {
         if (this.selectedLang === lang) {
@@ -230,20 +242,20 @@ Vue.use(VueTheMask);
       },
       closeModal() {
         this.showModal = false;
-        document.body.style.overflow = null;
+        this.setModalClosedClassToBody();
       },
       openModal() {
         this.showModal = true;
-        document.body.style.overflow = 'hidden';
+        this.setModalOpenedClassToBody();
       },
       closeSuccessModal() {
         this.showSuccess = false;
         this.request = 'clean';
-        document.body.style.overflow = null;
+        this.setModalClosedClassToBody();
       },
       openSuccessModal() {
         this.showSuccess = true;
-        document.body.style.overflow = 'hidden';
+        this.setModalOpenedClassToBody();
       },
       getWindowScrollTop() {
         this.windowScrollTop = window.scrollY;
