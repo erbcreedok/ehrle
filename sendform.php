1580b1cf8f540e246ba96ad4dc3caa3fe496ef43
yerbol <erbcreedok@gmail.com>
2020-01-31 18:14:59 +0600

    update tg text layout

--- sendform.php
+++ sendform.php
@@ -3,7 +3,7 @@ $q = $_SERVER['QUERY_STRING'];
 $url = "https://script.google.com/macros/s/AKfycbyMv9LcDQVTFb8Os4nQFONaxcYoLxEYyY_QqOawWXLtvHsqz8M0/exec?".$q;
 $tg_api = '1003245073:AAEgjRgpNBx0a9d47g6PSGU4_OQrCcW6ur8';
 $chat_id = '-1001313269104';
-$message = urlencode('Заявка: '.$_GET['name'].".\nТелефон: ".$_GET['phone']."\nФорма: ".$_GET['form']);
+$message = urlencode('Имя: '.$_GET['name'].";\nМоб. телефон: ".$_GET['phone']."\nОткуда: ".$_GET['form']);
 $url_tg = "https://api.telegram.org/bot$tg_api/sendMessage?chat_id=$chat_id&text=$message";
 $tg_response = file_get_contents($url_tg);
 $google_script_response = file_get_contents($url);
