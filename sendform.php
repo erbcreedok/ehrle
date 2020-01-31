0fbe7a18abb6fa0f31b040c0adef899073daf65e
yerbol <erbcreedok@gmail.com>
2020-01-31 15:21:52 +0600

    change telegram chat id

--- sendform.php
+++ sendform.php
@@ -2,7 +2,7 @@
 $q = $_SERVER['QUERY_STRING'];
 $url = "https://script.google.com/macros/s/AKfycbyMv9LcDQVTFb8Os4nQFONaxcYoLxEYyY_QqOawWXLtvHsqz8M0/exec?".$q;
 $tg_api = '1003245073:AAEgjRgpNBx0a9d47g6PSGU4_OQrCcW6ur8';
-$chat_id = '-386742703';
+$chat_id = '-1001313269104';
 $message = urlencode('Заявка: '.$_GET['name'].".\nТелефон: ".$_GET['phone']."\nФорма: ".$_GET['form']);
 $url_tg = "https://api.telegram.org/bot$tg_api/sendMessage?chat_id=$chat_id&text=$message";
 file_get_contents($url_tg);
